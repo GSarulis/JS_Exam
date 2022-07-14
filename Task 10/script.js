9b4a69e2859fd1c1853723fb779c895731d1b3bf
GSarulis <g.v.sarulis@gmail.com>
2022-07-14 15:17:32 +0300

    FINISHG EXAM

--- Task 10/script.js
+++ Task 10/script.js
@@ -2,6 +2,14 @@
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
 
+// import { composition } from "./modules/math/composition.js";
+// import { division } from "./modules/math/division.js";
+// import { substraction } from "./modules/math/subtraction.js";
+// import { multiplication } from "./modules/math/multiplication.js";
+// import { one, two, three, four, five } from "./modules/numbers.js";
+
+import { composition } from "./modules/math/composition.js";
+
 let a = composition(one, four);
 let b = division(four, two);
 let c = substraction(three, two);
@@ -11,30 +19,3 @@ console.log(a);
 console.log(b);
 console.log(c);
 console.log(d);
-
-const showUser = document.querySelector("#btn");
-
-const renderCards = (info) => {
-  const outputLogin = document.createElement("h3");
-  outputLogin.textContent = info.login;
-  const outputImg = (document.createAttribute("img").src = info.avatar_url);
-  // outputImg.src = info.avatar_url;
-  // outputImg.alt = info.avatar_url;
-
-  document.querySelector("#message").textContent = "";
-
-  const card = document.createElement("div");
-  card.style.width = "30%";
-  card.style.borderRadius = "12px";
-  card.style.padding = "12px";
-  card.style.margin = "12px";
-  card.style.display = "flex";
-  card.style.background = "#FC2E20";
-  card.style.boxShadow = "3px 6px 9px #FC2E20";
-
-  const output = document.querySelector("#output");
-  output.style.display = "flex";
-  output.style.flexWrap = "wrap";
-  card.append(outputLogin, outputImg);
-  output.append(card);
-};
--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,25 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive = () => {
+    return this.budget > 10000000;
+  };
+}
+const avatar = new Movie(
+  "Avatar: The Way of Water",
+  "James Cameron",
+  250000000
+);
+// console.log(avatar.wasExpensive());
+console.log(
+  `${avatar.title}, directed by ${
+    avatar.director
+  }, is expensive: ${avatar.wasExpensive(avatar)}`
+);
